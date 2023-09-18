f11a62171394ecef8e22770e6d53d537f725a817
Oleg Baskakov <obask@users.noreply.github.com>
2023-09-17 20:53:43 -0700

    fix Json config with autoreload

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,26 +3,22 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import io.ktor.server.plugins.callloging.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.plugins.contentnegotiation.*
 import kotlinx.serialization.json.Json
 
 fun main() {
-    val json = Json {
-//                useArrayPolymorphism = true
-                classDiscriminator = "_t"
-    }
     embeddedServer(Netty, port = 9000, host = "127.0.0.1") {
         install(ContentNegotiation) {
-            json(json)
+            json(Json {
+                classDiscriminator = "_t"
+            })
         }
-        install(CallLogging)
 
         routing {
             get("/") {
-                call.respondText("Hello World 6", status = HttpStatusCode.OK)
+                call.respondText("Hello World 5", status = HttpStatusCode.OK)
             }
             get("/book") {
                 val path = {}.javaClass.getResource("Dostoyevsky.txt")
